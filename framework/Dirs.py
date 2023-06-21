6d3832c644946b9cedd09ccfcf701c481c9c3493
Ariel Klinerman <arielkl999a@gmail.com>
2023-06-21 06:34:43 -0400

    Update Dirs.py
    added progress bar
    fixed only finds code 200 urls

--- framework/Dirs.py
+++ framework/Dirs.py
@@ -1,22 +1,36 @@
 import urllib.request
 import os
 import Misc
+from alive_progress import alive_bar
 
 class MyDirBuster:
     def my_dir_buster(domain): 
         path = Misc.Misc.make_dir("Dirs")
         with open(f"{os.getcwd()}/Lists/dirs.txt","r") as file:
-            with open(f"{path}/dirs.txt","w") as res:
-                print("Running Dir Brute Force Abuse...\n")
-                res.writelines("Running Dir Brute Force Abuse...\n\n")
-                print(f"Using List: {os.getcwd()}/Lists/dirs.txt\n")
-                res.writelines(f"Using List: {os.getcwd()}/Lists/dirs.txt\n")
-                for line in file:
-                    try:
-                        line = line.strip("\n")
-                        code = urllib.request.urlopen(f'{domain}/{line}').getcode()
-                        print(f"[+] {domain}/{line} CODE: {code}")
-                        res.writelines(f"[+] {domain}/{line} CODE: {code}\n")
-                    except Exception:
-                        pass
-
+            counter = 0
+            for line in file:
+                counter += 1
+        with open(f"{os.getcwd()}/Lists/dirs.txt","r") as file:
+            with alive_bar(counter) as bar:
+                with open(f"{path}/dirs.txt","w") as res:       
+                    print("Running Dir Brute Force Abuse...\n")
+                    res.writelines("Running Dir Brute Force Abuse...\n\n")
+                    print(f"Using List: {os.getcwd()}/Lists/dirs.txt\n")
+                    res.writelines(f"Using List: {os.getcwd()}/Lists/dirs.txt\n")
+                    for line in file:
+                        try:
+                            line = line.strip("\n")
+                            code = urllib.request.urlopen(f'{domain}/{line}').getcode()
+                            print(f"{domain}/{line} CODE: {code}")
+                            res.writelines(f"[+] {domain}/{line} CODE: {code}\n")
+                            bar()
+                        except Exception as e:
+                            e = str(e)
+                            code = e.split(" ")[2]
+                            code = code.strip(":")
+                            if code != "404":
+                                if code.isnumeric():
+                                    print(f"{domain}/{line} CODE: {code}")
+                                    res.writelines(f"[+] {domain}/{line} CODE: {code}\n")
+                            bar()
+                                
